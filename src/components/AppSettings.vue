1965d9c48fb7a77057ad0fd417ebb07315821ff3
Akash Singh <meteorain.dev@gmail.com>
2026-05-21 21:25:13 +0300

    Add AppSettings component
    - Display app version and commit SHA
    - Add button grid with GitHub link
    - Add discussions and licenses buttons
    - Use Volt Button components with icons
    - Prepare UI for settings expansion

---
+++ src/components/AppSettings.vue
@@ -0,0 +1,65 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+import Button from '@/volt/Button.vue'
+</script>
+
+<template>
+  <div
+    class="text-base sm:text-lg text-gray-500 text-center mt-8 grid grid-cols-2 grid-rows-2 gap-2 wrap-anywhere items-center"
+  >
+    <Button
+      variant="outlined"
+      :label="`Version: ${versionInfo.version}`"
+      class="h-[75px] w-[100%]"
+    >
+      <template #icon>
+        <Icon icon="mdi:information-variant" width="30" /></template
+    ></Button>
+    <Button variant="outlined" label="Github" class="h-[75px] w-[100%]">
+      <template #icon> <Icon icon="mdi:github" width="20" /></template
+    ></Button>
+
+    <Button
+      variant="outlined"
+      :label="`${versionInfo.sha.slice(0, 7)}...`"
+      class="h-[75px] w-[100%]"
+    >
+      <template #icon> <Icon icon="mdi:source-commit" width="20" /></template
+    ></Button>
+
+    <Button
+      variant="outlined"
+      label="Share your ideas"
+      class="h-[75px] w-[100%]"
+    >
+      <template #icon> <Icon icon="mdi:discussion" width="20" /></template
+    ></Button>
+    <Button variant="outlined" label="Licenses" class="h-[75px] w-[100%]">
+      <template #icon> <Icon icon="mdi:license" width="20" /></template
+    ></Button>
+  </div>
+</template>
+
+<style scoped>
+.version-footer {
+  margin-top: 3rem;
+  font-size: 0.75rem;
+  color: #666;
+  text-align: center;
+}
+.version-footer a {
+  color: #0353a4;
+  text-decoration: none;
+}
+.version-footer a:hover {
+  text-decoration: underline;
+}
+
+.icon-inline {
+  display: inline;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+</style>
